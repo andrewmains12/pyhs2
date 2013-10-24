4e1734c9cc6349b1f2496271628b4375bb3c3fb1
andrew-mains <andrew.mains@kontagent.com>
2013-10-24 01:39:05 +0000

    Updated example; made tests run properly using python setup.py test

--- example.py
+++ example.py
@@ -1,14 +1,12 @@
 import pyhs2
 
-conn = pyhs2.connect(host='localhost', 
-					port=10000,
-					authMechanism="PLAIN", 
-					user='root', 
-					password='test', 
-					database='default')
-cur = conn.cursor()
-cur.execute("show tables")
-for i in cur.fetch():
-	print i
-cur.close()
-conn.close()
+with pyhs2.connect(host='localhost',
+                   port=10000,
+                   authMechanism="PLAIN",
+                   user='root',
+                   password='test',
+                   database='default') as conn:
+    with conn.cursor() as cur:
+        cur.execute("show tables")
+        for i in cur.fetch():
+            print i
